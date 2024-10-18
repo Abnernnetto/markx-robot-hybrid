93d250ae895361372eeb43f708f04fce80f4a995
Abner Nunes Netto <60905173+Abnernnetto@users.noreply.github.com>
2024-10-18 13:34:04 -0300

    Update mongo.py

--- resources/libs/mongo.py
+++ resources/libs/mongo.py
@@ -1,7 +1,7 @@
 from robot.api.deco import keyword
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://abnernnetto:Inter1988@cluster0.ju1dtos.mongodb.net/markX?retryWrites=true&w=majority&appName=Cluster0', tls=True, tlsAllowInvalidCertificates=True)
+client = MongoClient('mongodb+srv://user:password@cluster0.ju1dtos.mongodb.net/markX?retryWrites=true&w=majority&appName=Cluster0', tls=True, tlsAllowInvalidCertificates=True)
 
 db = client['markX']
 
